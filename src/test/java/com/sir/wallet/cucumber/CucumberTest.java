24d04dd11ebddf90e8bce7bc5f4626032e3f5553
filaa <filassira@gmail.com>
2023-01-25 23:16:53 +0000

    Implémentation du scénario TransactionStepDefs

--- src/test/java/com/sir/wallet/cucumber/CucumberTest.java
+++ src/test/java/com/sir/wallet/cucumber/CucumberTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
+@CucumberOptions(features = "src/test/resources/")
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
