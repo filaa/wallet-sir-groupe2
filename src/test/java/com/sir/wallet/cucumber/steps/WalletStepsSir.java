ff542cef889be7791e10a7577c4b5fb443f3f8f8
Palaye12 <palayesambinfo@gmail.com>
2023-01-26 18:15:34 +0000

    reviews code WalletStepsSir....

--- src/test/java/com/sir/wallet/cucumber/steps/WalletStepsSir.java
+++ src/test/java/com/sir/wallet/cucumber/steps/WalletStepsSir.java
@@ -20,8 +20,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -32,6 +31,7 @@ public class WalletStepsSir {
     private final RestTemplate restTemplate;
     private Wallet wallet;
 private ResponseEntity<Wallet> response;
+    private ResponseEntity<Wallet> response1;
 
 private ResponseEntity<List<Wallet>> listresponse;
 
@@ -58,38 +58,47 @@ private Wallet wallets;
       response=walletController.saveWallet(wallet);
 
     }
-    @Then("the response status should be {int}")
-    public void checkResponseStatus(int status) {
+    @Then("the response status should be {long}")
+    public void checkResponseStatus(long status) {
 
         assertEquals(status, response.getStatusCode().value());
+
     }
     @And("the response body should contain the wallet")
     public void checkResponseBody() {
         Wallet personResponse = response.getBody();
         assertNotNull(personResponse);
+
+
     }
 
-    @Given("a wallet with ID {int} and name {string} and balance {int}")
+    @Given("a wallet with ID {long} and name {string} and balance {long}")
     public void createWallet(long id, String name, long balance) {
-         wallet = new Wallet(id, name, balance);
+        wallet = new Wallet(id, name, balance);
     }
 
     @When("I GET the wallet from the {string} endpoint")
     public void getWallet(String endpoint) {
-        String url = "http://localhost:8080/api/wallet/" + endpoint + "/{id}";
-        response = restTemplate.getForEntity(url, Wallet.class, wallet.getId());
-    }
-    @When("I PUT the wallet to the {string} endpoint with name {string} and balance {int}")
-    public void updateWallet(String endpoint, String name, int balance) {
-        wallet.setBalance(balance);
-        wallet.setName(name);
-        response = restTemplate.exchange(endpoint, HttpMethod.PUT, new HttpEntity<>(wallet), Wallet.class);
+        String url = "http://localhost:8080/api/wallet/"+endpoint;
+        response = restTemplate.getForEntity(url, Wallet.class);
     }
 
+
+    @When("I PUT the wallet to the {string} endpoint with name {string} and balance {long}")
+    public void updateWallet(String endpoint, String name, long balance) {
+        wallet.setBalance(balance);
+        wallet.setName(name);
+        String url = "http://localhost:8080/api/wallet/"+endpoint;
+        response = restTemplate.exchange(url, HttpMethod.PUT, new HttpEntity<>(wallet), Wallet.class);
+    }
     @And("the response body should contain the updated wallet")
     public void checkUpdateResponseBody() {
-        Wallet wallets = response.getBody();
-        assertEquals("Updated Wallet",wallets.getName());
-        assertEquals(200,wallets.getBalance());
+        Wallet updatedWallet = response.getBody();
+
+            assertEquals(wallet.getName(), updatedWallet.getName());
+            assertEquals(wallet.getBalance(), updatedWallet.getBalance());
+
+
     }
+
 }
